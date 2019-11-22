0909543de5753b3ddcb52305b5f22ce0386ac6cd
Frenzoid <frenzoid@protonmail.com>
2019-11-22 14:47:41 +0100

    Update entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -3,8 +3,8 @@
 if [[ $UPDATE == true || $UPDATE == "true" || $UPDATE == 1 ]]
   then
     mkdir -p "/home/steamsrv/dayofdragons_server/"
-    /home/steamsrv/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steamsrv/dayofdragons_server/ +app_update 1088320 validate $STEAMCMD +quit
+    /home/steamsrv/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steamsrv/dayofdragons_server/ +app_update 1088320 validate $STEAMPARAMS +quit
 fi
 
 chmod 775 /home/steamsrv/dayofdragons_server/DragonsServer.sh
-/home/steamsrv/dayofdragons_server/DragonsServer.sh
+/home/steamsrv/dayofdragons_server/DragonsServer.sh $SERVERPARAMS
