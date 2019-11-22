459b0bfffa1e9e004cbc1bed1196a4b47fca6b1f
Frenzoid <frenzoid@protonmail.com>
2019-11-22 12:18:41 +0100

    added exec flag

--- entry-point.sh
+++ entry-point.sh
@@ -6,4 +6,5 @@ if [ $UPDATE == true || $UPDATE == "true" || $UPDATE == 1 ]
     /home/steamsrv/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steamsrv/dayofdragons_server/ +app_update 1088320 validate +quit
 fi
 
+chmod 775 /home/steamsrv/dayofdragons_server/DragonsServer.sh
 /home/steamsrv/dayofdragons_server/DragonsServer.sh
