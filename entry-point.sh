f587171231b0a80673579c323a3f23cf21c9eb6f
Frenzoid <frenzoid@protonmail.com>
2019-12-03 16:13:36 +0100

    Update entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -11,6 +11,13 @@ if [[ $EULA == "accept" || $EULA == "ACCEPT" || $EULA == "true" || $EULA == "TRU
       /home/steamsrv/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steamsrv/dayofdragons_server/ +app_update 1088320 validate $STEAMPARAMS +quit
   fi
 
+  # Updates the Game.ini file.
+  sed -i 's/steamAdminId/$ADMINSTEAMID/g' /home/steamsrv/predodconfig/Game.ini
+  sed -i 's/maxPlayersValue/$MAXPLAYERS/g' /home/steamsrv/predodconfig/Game.ini
+  sed -i 's/whiteListBoolean/$WHITELIST/g' /home/steamsrv/predodconfig/Game.ini
+  sed -i 's/autoSaveTimerS/$AUTOSAVESECONDS/g' /home/steamsrv/predodconfig/Game.ini
+  cp /home/steamsrv/predodconfig/Game.ini /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Game.ini
+
   # Launches the DoD server.
   chmod 775 /home/steamsrv/dayofdragons_server/DragonsServer.sh
   /home/steamsrv/dayofdragons_server/DragonsServer.sh $SERVERPARAMS
