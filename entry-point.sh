33a4365da6bf1415351c02be2817645d7ed35596
Frenzoid <frenzoid@protonmail.com>
2019-12-04 16:14:25 +0100

    Update entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -11,27 +11,11 @@ if [[ $EULA == "accept" || $EULA == "ACCEPT" || $EULA == "true" || $EULA == "TRU
       /home/steamsrv/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steamsrv/dayofdragons_server/ +app_update 1088320 validate $STEAMPARAMS +quit
       chmod 775 /home/steamsrv/dayofdragons_server/Dragons/Binaries/Linux/DragonsServer-Linux-Shipping
   fi
-
-  # Checks if the server config needs to be updated. (CURRENTLY NOT APPLICABLE)
-  if [[ $UPDATECONFIG == true || $UPDATECONFIG == "true" || $UPDATECONFIG == 1 ]]
-    then 
-      # Updates the Game.ini file.
-      mkdir -p /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/
-      cp /home/steamsrv/predodconfig/Game.ini /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Game.ini
-      sed -i "s/steamAdminId/$ADMINSTEAMID/g" /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Game.ini
-      sed -i "s/maxPlayersValue/$MAXPLAYERS/g" /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Game.ini
-      sed -i "s/whiteListBoolean/$WHITELIST/g" /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Game.ini
-      sed -i "s/autoSaveTimerS/$AUTOSAVESECONDS/g" /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Game.ini
-
-      # Copies the Engine.ini file.
-      cp /home/steamsrv/predodconfig/Engine.ini /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Engine.ini 
-  fi
   
   # Launches the DoD server.
   /home/steamsrv/dayofdragons_server/Dragons/Binaries/Linux/DragonsServer-Linux-Shipping -SteamServerName=$SERVERNAME $SERVERPARAMS
 
   else
-
     # If EULA was not accepted, show a message, and close the container.
     echo "EULA was not accepted!, current value: $EULA";
     exit 1;
