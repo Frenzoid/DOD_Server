fccdfbda27e40b43d4f827beb23e448b537b9dca
Frenzoid <frenzoid@protonmail.com>
2019-12-04 07:19:32 +0100

    Update entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -9,18 +9,20 @@ if [[ $EULA == "accept" || $EULA == "ACCEPT" || $EULA == "true" || $EULA == "TRU
     then
       mkdir -p "/home/steamsrv/dayofdragons_server/"
       /home/steamsrv/steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/steamsrv/dayofdragons_server/ +app_update 1088320 validate $STEAMPARAMS +quit
+      chmod 775 /home/steamsrv/dayofdragons_server/DragonsServer.sh
   fi
 
   # Updates the Game.ini file.
-  cp /home/steamsrv/predodconfig/Engine.ini /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Engine.ini 
   cp /home/steamsrv/predodconfig/Game.ini /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Game.ini
   sed -i "s/steamAdminId/$ADMINSTEAMID/g" /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Game.ini
   sed -i "s/maxPlayersValue/$MAXPLAYERS/g" /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Game.ini
   sed -i "s/whiteListBoolean/$WHITELIST/g" /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Game.ini
   sed -i "s/autoSaveTimerS/$AUTOSAVESECONDS/g" /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Game.ini
+  
+  # Copies the Engine.ini file.
+  cp /home/steamsrv/predodconfig/Engine.ini /home/steamsrv/dayofdragons_server/Dragons/Saved/Config/LinuxServer/Engine.ini 
 
   # Launches the DoD server.
-  chmod 775 /home/steamsrv/dayofdragons_server/DragonsServer.sh
   /home/steamsrv/dayofdragons_server/DragonsServer.sh SteamServerName=$SERVERNAME $SERVERPARAMS
 
   else
