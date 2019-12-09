0e68ae1c2a12aecefb6dc44d6020626e20f2ac67
Frenzoid <frenzoid@protonmail.com>
2019-12-09 09:11:41 +0100

    Update launchserver.sh

--- launchserver.sh
+++ launchserver.sh
@@ -1,1 +1,1 @@
-/home/steamsrv/dayofdragons_server/Dragons/Binaries/Linux/DragonsServer-Linux-Shipping -SteamServerName=$SERVERNAME $SERVERPARAMS
+/home/steamsrv/dayofdragons_server/Dragons/Binaries/Linux/DragonsServer-Linux-Shipping -SteamServerName=$SERVERNAME $SERVERPARAMS & $! > DragonsPid.txt
